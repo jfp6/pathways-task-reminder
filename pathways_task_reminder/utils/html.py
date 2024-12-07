fd216fd9a55b98c48c8148f0d692ea91de42f0b3
John T. Prince <jtprince@gmail.com>
2024-12-07 13:54:17 -0700

    using html 2 image

--- pathways_task_reminder/utils/html.py
+++ pathways_task_reminder/utils/html.py
@@ -1,17 +1,21 @@
 from pathlib import Path
-import subprocess
+from html2image import Html2Image
 
 
 def to_image_path(inpath: Path, outpath: Path):
-    cmd = [
-        "wkhtmltoimage",
-        "--width",
-        str(400),
-        "--format",
-        outpath.suffix[1:],
-        str(inpath),
-        str(outpath),
-    ]
-    subprocess.run(cmd)
-    assert outpath.exists()
+    html_to_image = Html2Image(output_path=str(outpath.parent), size=(400, 200))
+    html_to_image.screenshot(html_file=str(inpath), save_as=outpath.name)
     return outpath
+
+    # cmd = [
+    #     "wkhtmltoimage",
+    #     "--width",
+    #     str(400),
+    #     "--format",
+    #     outpath.suffix[1:],
+    #     str(inpath),
+    #     str(outpath),
+    # ]
+    # subprocess.run(cmd)
+    # assert outpath.exists()
+    # return outpath
